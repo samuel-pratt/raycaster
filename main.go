e60eddb0e3b8c565986cddcd516049fececb998e
Sam Pratt <sampratt@Sams-MacBook-Pro.local>
2021-10-11 14:33:25 -0700

    Added player, map, and movement

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var (
+	px       float64 = 300 // Player X Coordinate
+	py       float64 = 300 // Player Y Coordinate
+	pa       float64 = 0   // Player Angle
+	mapx     int     = 8   // Map Width
+	mapy     int     = 8   // Map Height
+	mapScale int     = 64  // Map Unit Size
+	mapArray         = [64]int{
+		1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 1, 0, 0, 0, 0, 1,
+		1, 0, 1, 0, 0, 0, 0, 1,
+		1, 0, 1, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 1, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 1,
+	}
+	boot int = 0
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	KeyboardHandler()
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	DrawMap(screen)
+	ebitenutil.DrawRect(screen, px, py, 3, 3, color.White)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(1024, 512)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
